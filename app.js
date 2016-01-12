10f8ffdcf9c8a9eb39fce80bdd0db81f8765badb
Julia Pearson <julia.pearson@gmail.com>
2016-01-12 16:04:28 -0500

    got working calendar with clickable data-seeded events

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ var express = require('express');
 var app = express();
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override')
+var methodOverride = require('method-override');
+var moment = require('moment');
 
 var urlencodedBodyParser = bodyParser.urlencoded({extended:false});
 app.use(urlencodedBodyParser);
 
-app.use(express.static('_dirname'+'/public'))
+app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname));
+app.use(express.static(__dirname+'/bower_components'));
 
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
-var mongoUrl = "mongodb://localhost:27017/myDb";
+var mongoUrl = "mongodb://localhost:27017/myDB";
 var db;
 
 MongoClient.connect(mongoUrl, function(err, database){
@@ -22,6 +25,8 @@ MongoClient.connect(mongoUrl, function(err, database){
   db = database;
   process.on('exit', db.close);
 })
+
+
 
 
 app.set('view engine', 'ejs');
@@ -37,6 +42,13 @@ app.get('/calendar', function(req, res){
 
 app.get('/calendar/helmets', function(req, res){
   res.render('calendar');
+});
+
+app.get('/calendar/events', function(req,res){
+  console.log('ajax worked');
+  db.collection('events').find({}).toArray(function(error, results){
+    res.json(results);
+  })
 });
 
 
--- public/main.js
+++ public/main.js
@@ -1,1 +1,39 @@
-// console.log('linked')
+
+
+$(document).ready(function() {
+
+  $('#calendar').fullCalendar({
+    eventClick: function(calEvent) {
+      alert('Event: ' + calEvent.title);
+    }
+  });
+  var source = {
+    events: [
+          // {
+          //     title: 'Event1',
+          //     start: '2016-01-04'
+          // },
+          // {
+          //     title: 'Event2',
+          //     start: '2016-01-05',
+          //     end: '2016-01-07'
+          // },
+          // {
+          //     title: 'Event3',
+          //     start: '2016-01-05',
+          //     end: '2016-01-10'
+          // } 
+      ],
+    color: 'yellow',   
+    textColor: 'black' 
+  };
+  $.ajax({
+    url: '/calendar/events',
+    type: 'GET',
+    dataType: 'json'
+  }).done(function(response){
+    source.events = response;
+    $('#calendar').fullCalendar('addEventSource', source);
+  });
+
+});
