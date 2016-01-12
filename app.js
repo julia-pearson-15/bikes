b1b2635efc35e3260df580a6d0b692ea208fceb9
Julia Pearson <julia.pearson@gmail.com>
2016-01-12 12:09:01 -0500

    start up

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
-var expres = require('express');
+var express = require('express');
 var app = express();
 var ejs = require('ejs');
-var fs = require('fs');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override')
 
-app.use(express.static('_dirname'+'/public'))
 var urlencodedBodyParser = bodyParser.urlencoded({extended:false});
 app.use(urlencodedBodyParser);
 
+app.use(express.static('_dirname'+'/public'))
+
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+var mongoUrl = "mongodb://localhost:27017/myDb";
+var db;
+
+MongoClient.connect(mongoUrl, function(err, database){
+  if (err) {
+    console.log(err);
+  }
+  console.log("connected!");
+  db = database;
+  process.on('exit', db.close);
+})
+
+
 app.set('view engine', 'ejs');
+app.use(methodOverride('_method'));
 
 app.get('/', function(req,res){
-  res.send('hello World');
+  res.render('index');
 });
 
-app.listen(3000);
+app.get('/calendar', function(req, res){
+  res.render('calendar');
+});
+
+app.get('/calendar/helmets', function(req, res){
+  res.render('calendar');
+});
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000);
